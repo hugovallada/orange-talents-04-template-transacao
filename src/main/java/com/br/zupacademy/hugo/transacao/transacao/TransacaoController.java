406ba808cf59eddaf8d773d4a8a5b56395007c1d
hugovallada <valladahugo@gmail.com>
2021-05-18 15:16:04 -0300

    Remove imports desnecessários

--- src/main/java/com/br/zupacademy/hugo/transacao/transacao/TransacaoController.java
+++ src/main/java/com/br/zupacademy/hugo/transacao/transacao/TransacaoController.java
@@ -1,9 +1,6 @@
 package com.br.zupacademy.hugo.transacao.transacao;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +20,7 @@ public class TransacaoController {
     }
 
     @GetMapping("/{idCartao}")
-    public ResponseEntity<List<TransacaoResponse>> consultarPorCartao(@PathVariable String idCartao,
-                                                                      @AuthenticationPrincipal Jwt auth){
+    public ResponseEntity<List<TransacaoResponse>> consultarPorCartao(@PathVariable String idCartao){
 
         if(!transacoRepository.existsByCartaoId(idCartao)){
             return ResponseEntity.notFound().build();
