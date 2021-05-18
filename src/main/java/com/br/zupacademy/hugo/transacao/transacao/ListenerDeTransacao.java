abd04b931e5a22fb1168939fb9f3058090aa0399
hugovallada <valladahugo@gmail.com>
2021-05-18 07:52:11 -0300

    Define o listener de transações

---
+++ src/main/java/com/br/zupacademy/hugo/transacao/transacao/ListenerDeTransacao.java
@@ -0,0 +1,15 @@
+package com.br.zupacademy.hugo.transacao.transacao;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListenerDeTransacao {
+
+    @KafkaListener(topics = "${spring.kafka.topic.transactions}")
+    public void ouvir(EventoDeTransacao eventoDeTransacao){
+        System.out.println(eventoDeTransacao);
+    }
+
+
+}
